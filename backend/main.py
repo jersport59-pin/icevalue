7510085fc7d93d1d3922f678aef119f7ab2d0de5
jersport59-pin <jersport59@gmail.com>
2026-01-07 21:41:52 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="IceValue API")
+
+# Autoriser les sites web à appeler l’API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # autorise tous les sites (ok pour commencer)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
